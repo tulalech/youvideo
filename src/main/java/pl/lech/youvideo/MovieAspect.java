b719607a5ecaba87b5d48554896c5f05d50c91bc
lesio <lech.tula@onet.eu>
2021-05-18 14:19:15 +0200

    MailBox with JavaMailSender

--- src/main/java/pl/lech/youvideo/MovieAspect.java
+++ src/main/java/pl/lech/youvideo/MovieAspect.java
@@ -2,14 +2,33 @@ package pl.lech.youvideo;
 
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @Component
 @Aspect
 public class MovieAspect {
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMail() {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setFrom("lech.tula@gmail.com");
+        mail.setTo("lech.tula@onmail.com");
+        mail.setSubject("I have add a movie!");
+        mail.setText("Hi! You have a new movie in our library.");
+        mailSender.send(mail);
+    }
+
     @After("execution(* pl.lech.youvideo.MovieApi.addMovie(..))")
     private void afterAddMovie() {
         System.out.println("Wysłałem maila.");
+        sendMail();
     }
 }
